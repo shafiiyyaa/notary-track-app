1fe0dd4c3cab322767ba2beb03fc8dcec62fa277
NamaKamu <shafiyyahalkhansa553@gmail.com>
2026-06-07 08:43:45 +0700

    splashscreen

--- lib/features/onboarding/view/splash_screen.dart
+++ lib/features/onboarding/view/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../../../constants/constants.dart';
 import 'onboarding_screen.dart';
 
 class CustomSplashScreen extends StatefulWidget {
@@ -14,7 +13,7 @@ class _CustomSplashScreenState extends State<CustomSplashScreen> {
   @override
   void initState() {
     super.initState();
-    //  jeda 3 detik lalu pindah ke halaman Onboarding
+    // Jeda 3 detik lalu pindah ke halaman Onboarding
     Future.delayed(const Duration(seconds: 3), () {
       if (mounted) {
         Navigator.pushReplacement(
@@ -28,27 +27,29 @@ class _CustomSplashScreenState extends State<CustomSplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 91, 141, 184),
+     
+      backgroundColor: const Color(0xFF5B8DB8),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'NotaryTrack',
-              style: GoogleFonts.comfortaa(
-                fontSize: 48,
-                fontWeight: FontWeight.bold,
-                color: AppColors.primaryBlueDark,
-                letterSpacing: 2.0,
-              ),
+            // 1. Bagian Logo Gambar
+            Image.asset(
+              'assets/images/logo.png',
+              width: 180, // ukuran lebar logo di sini
+              height: 180, // ukuran tinggi logo di sini
+              fit: BoxFit.contain,
             ),
-            const SizedBox(height: 12),
+
+            const SizedBox(height: 16), // Jarak antara logo dan tulisan teks
+            // 2. Bagian Tulisan "Notary Track" di bawah logo
             Text(
-              'Document Monitoring Application',
-              style: GoogleFonts.plusJakartaSans(
-                fontSize: 14,
-                color: Colors.black45,
+              'Notary Track',
+              style: GoogleFonts.cinzel(
+                fontSize: 32,
                 fontWeight: FontWeight.w500,
+                color: const Color(0xFFEFECE3),
+                letterSpacing: 1.2,
               ),
             ),
           ],
